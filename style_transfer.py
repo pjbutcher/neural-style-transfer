90198bfc7e4613cd6575e1e85eef980d4b9ca85f
Phil Butcher <phillipjbutcher@gmail.com>
2017-05-23 19:05:21 -0700

    script can now be called from the command line

--- style_transfer.py
+++ style_transfer.py
@@ -1,3 +1,4 @@
+import argparse
 import numpy as np
 from keras import backend as K
 from keras import metrics
@@ -10,14 +11,9 @@ from scipy.ndimage.filters import gaussian_filter
 from vgg16_avg import VGG16_Avg
 
 
-rn_mean = np.array([123.68, 116.779, 103.939], dtype=np.float32)
-preproc = lambda x: (x - rn_mean)[:, :, :, ::-1]
-deproc = lambda x,s: np.clip(x.reshape(s)[:, :, :, ::-1] + rn_mean, 0, 255)
-
-
 class Evaluator(object):
     def __init__(self, f, shp):
-        self.f = f 
+        self.f = f
         self.shp = shp
         
     def loss(self, x):
@@ -37,14 +33,17 @@ def solve_image(eval_obj, niter, x, shp):
         )
         x = np.clip(x, -127, 127)
         print('Current loss value:', min_val)
-        imsave('results/res_at_iteration_{}.png'.format(i), deproc(x.copy(), shp)[0])
+        imsave(
+            'results/res_at_iteration_{}.png'.format(i), 
+            deprocess(x.copy(), rn_mean, shp)[0]
+        )
     return x
 
 
 def gram_matrix(x):
     # We want each row to be a channel, and the columns to be flattened x,y locations
     features = K.batch_flatten(K.permute_dimensions(x, (2, 0, 1)))
-    # The dot product of this with its transpose shows the correlation 
+    # The dot product of this with its transpose shows the correlation
     # between each pair of channels
     return K.dot(features, K.transpose(features)) / x.get_shape().num_elements()
 
@@ -59,17 +58,39 @@ def random_image(shape):
     return np.random.uniform(-2.5, 2.5, shape) / 100
 
 
+def preprocess(img_arr, img_mean):
+    return (img_arr - img_mean)[:, :, :, ::-1]
+
+
+def deprocess(img_arr, img_mean, shape):
+    return np.clip(img_arr.reshape(shape)[:, :, :, ::-1] + img_mean, 0, 255)
+
+
+# parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('content')
+parser.add_argument('style')
+parser.add_argument('--output_final')
+parser.add_argument('--output_iters')
+args = parser.parse_args()
+
+# mean from VGG authors used to preprocess images
+rn_mean = np.array([123.68, 116.779, 103.939], dtype=np.float32)
+
+# style_wgts = [0.05, 0.2, 0.2, 0.25, 0.3]
+style_wgts = [0.02, 0.1, 0.1, 0.15, 0.15]
+iterations = 20
+
 # load and process the content image
-content_img = Image.open('suicide-girl-2.jpg')
-content_arr = preproc(np.expand_dims(np.array(content_img), 0))
+content_img = Image.open(args.content)
+content_arr = preprocess(np.expand_dims(np.array(content_img), 0), rn_mean)
 content_shape = content_arr.shape
 
 # load and process the style image
-style_img = Image.open('snake.jpg')
+style_img = Image.open(args.style)
 style_img = style_img.resize((content_shape[2], content_shape[1]))
-style_arr = preproc(np.expand_dims(style_img, 0)[:, :, :, :3])
+style_arr = preprocess(np.expand_dims(style_img, 0)[:, :, :, :3], rn_mean)
 style_shape = style_arr.shape
-
 
 model = VGG16_Avg(include_top=False, input_shape=style_shape[1:])
 outputs = {l.name: l.output for l in model.layers}
@@ -83,8 +104,6 @@ style_targs = [K.variable(o) for o in style_model.predict(style_arr)]
 content_model = Model(model.input, content_layer)
 content_targ = K.variable(content_model.predict(content_arr))
 
-# style_wgts = [0.05, 0.2, 0.2, 0.25, 0.3]
-style_wgts = [0.02, 0.1, 0.1, 0.15, 0.15]
 
 loss = sum(style_loss(l1[0], l2[0])*w
            for l1, l2, w in zip(style_layers, style_targs, style_wgts))
@@ -94,7 +113,28 @@ transfer_fn = K.function([model.input], [loss]+grads)
 
 evaluator = Evaluator(transfer_fn, style_shape)
 
-iterations = 20
-x = rand_img_100(style_shape)
+styled_img = random_image(style_shape)
+for i in range(iterations):
+    styled_img, min_val, info = fmin_l_bfgs_b(
+        evaluator.loss,
+        styled_img.flatten(),
+        fprime=evaluator.grads,
+        maxfun=20
+    )
+    styled_img = np.clip(styled_img, -127, 127)
+    print('Current loss value: {} (iteration {}/{})'.format(
+        min_val, i+1, iterations))
 
-x = solve_image(evaluator, iterations, x, style_shape)
+    if args.output_iters is not None:    
+        imsave(
+            '{}/iteration_{}.png'.format(args.output_iters, i),
+            deprocess(styled_img.copy(), rn_mean, style_shape)[0]
+        )
+
+if args.output_final is not None:
+    imsave(
+        '{}.png'.format(args.output_final),
+        deprocess(styled_img.copy(), rn_mean, style_shape)[0]
+    )
+
+# x = solve_image(evaluator, iterations, random_img, style_shape)
